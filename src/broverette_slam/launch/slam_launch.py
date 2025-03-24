43047ba0ebb6f7bfa13dbf4711597488c2a2ecef
Ali Albustami <alibustami30@gmail.com>
2025-03-24 03:54:33 -0400

    change the model loaded to RVIZ to the latest urdf file

--- src/broverette_slam/launch/slam_launch.py
+++ src/broverette_slam/launch/slam_launch.py
@@ -10,12 +10,39 @@ def generate_launch_description():
     slam_toolbox_launch_dir = os.path.join(
         get_package_share_directory('slam_toolbox'), 'launch', 'online_async_launch.py')
 
+    params_file = os.path.join(
+        get_package_share_directory('broverette_slam'), 'config', 'params.yaml'
+    )
+
     # Include the SLAM Toolbox's online async launch file
     slam_toolbox_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(slam_toolbox_launch_dir),
         launch_arguments={
+            # 'base_frame': 'base_link',
+            # 'odom_frame': 'odom',
+            'slam_params_file': params_file,
             'use_sim_time': 'false',  # Adjust based on your needs
         }.items(),
+    )
+
+    urdf_file = os.path.join(
+        get_package_share_directory('broverette_description'),
+        'urdf',
+        'broverette.urdf'
+    )
+
+    with open(urdf_file, 'r') as infp:
+        robot_description = infp.read()
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[
+            {
+                'robot_description': robot_description,
+            }
+        ]
     )
 
     # Add the path to your existing broverette RViz config file from broverette_description
@@ -33,5 +60,6 @@ def generate_launch_description():
 
     return LaunchDescription([
         slam_toolbox_launch,
-        rviz_node
+        robot_state_publisher_node,
+        rviz_node,
     ])
